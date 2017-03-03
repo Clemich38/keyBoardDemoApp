944328d4bb26d08724297aabbfd48cf784bd3018
Clemich38 <clement.letreguilly@gmail.com>
2017-03-03 12:06:38 +0900

    add comments

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,33 +8,35 @@ import { CustomKeyBoard } from '../../components/customKeyBoard/custom-keyboard'
 })
 export class HomePage {
 
+  // Hexadecimal input string
   public hexStr: string;
 
   constructor(public navCtrl: NavController)
   {
     this.hexStr = "";
 
-    // Subscribe to the click event observable    
+    // Subscribe to the click event observable
+    // Here we add the clicked key value to the string 
     CustomKeyBoard.onCKClick.subscribe((key) => {
       this.hexStr += key;
     })
 
-    // Subscribe to the delete event observable    
+    // Subscribe to the delete event observable 
+    // Here we delete the last character of the string   
     CustomKeyBoard.onDeleteClick.subscribe(() => {
       this.hexStr = this.hexStr.slice(0, this.hexStr.length -1);
     })
   }
 
+  // Show keyboard button
   public showKeyboard()
   {
     CustomKeyBoard.show();
   }
 
+  // Hide keyboard function
   public hideKeyboard()
   {
     CustomKeyBoard.hide();
   }
-
-
-
 }
